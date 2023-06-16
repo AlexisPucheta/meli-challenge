51bb6ab0ba21975c2cab7bcf3efe35929f07632c
Alexis Pucheta <puche@Alexiss-MacBook-Pro.local>
2023-06-16 20:33:13 -0300

    mono repo created. next app created. server express created

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(3001, ()=> {
+    console.log("Server running on port 3001")
+});
+
+app.get('/', (_req,res) => {
+    res.send('Hello World!')
+});
